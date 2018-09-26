ce1159bf9af8cda23b3a4629d6ce94bf0057312f
Yerkebulan <erke.zhumagali@gmail.com>
2018-09-26 19:58:16 +0600

    Week 4

--- Week 4/TODO/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Author, Task
-
-admin.site.register(Author)
-admin.site.register(Task)
--- Week 4/todo/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from main import views
-
-urlpatterns = [
-    path('todos/', views.todos),
-    re_path(r'^todos/(\d+)/completed/$', views.completed_todos),
-]
