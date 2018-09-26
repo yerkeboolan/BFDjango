2d938dc77800021553646805cdaec076805aceaf
Yerkebulan <erke.zhumagali@gmail.com>
2018-09-26 19:27:55 +0600

    Week 4 | Models, The Django Administration Site

--- Week 4/TODO/main/views.py
+++ Week 4/TODO/main/views.py
@@ -1,3 +1,31 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
 
-# Create your views here.
+
+def todos(request):
+    current_day = datetime.today()
+    todo_list = [dict(title="Task {}".format(i), created=current_day.strftime("%d/%m/%y"),
+                      due_on=(current_day + timedelta(days=3)).strftime("%d/%m/%y"), owner="admin", mark=True)
+                 for i in range(1, 5)
+                 ]
+
+    context = {
+        'todo_list': todo_list,
+    }
+
+    return render(request, 'todo_list.html', context)
+
+
+def completed_todos(request, index):
+    cur_day = datetime.today()
+    task = {
+        'title': "Task {}".format(index),
+        'created': cur_day.strftime("%d/%m/%y"),
+        'due_on': (cur_day + timedelta(days=3)).strftime("%d/%m/%y"),
+        'owner': "admin",
+        'mark': False
+    }
+    context = {
+        'task': task
+    }
+    return render(request, 'completed_todo_list.html', context)
